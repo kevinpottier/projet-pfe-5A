aa1157fef72aa9b353fc4cda161145e46db4f8ed
Kévin Pottier <kevin.pottier@orange.fr>
2017-12-11 12:19:56 +0100

    Init

---
+++ src/projet_airfrance/Projet_AirFrance.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_airfrance;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Projet_AirFrance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
